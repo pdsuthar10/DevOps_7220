180a97b9a2e8453e86dcff3b95826e4165fe003e
Priyam Suthar <suthar.p@northeastern.edu>
2021-10-03 15:26:15 -0400

    Add API to return sentiment analysis response

---
+++ assignment3/part2-sentiment-analysis/backend/src/main/java/com/csye7220/assignment3webapp/controller/SentimentController.java
@@ -0,0 +1,34 @@
+package com.csye7220.assignment3webapp.controller;
+
+import com.csye7220.assignment3webapp.model.Sentence;
+import com.csye7220.assignment3webapp.model.Sentiment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class SentimentController {
+    @Value("${assignment3webapp.logic.api}")
+    private String logicApi;
+
+    @PostMapping("/sentiment")
+    public Sentiment sentimentAnalysis(@RequestBody Sentence sentence) {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.postForEntity(logicApi + "/analyse/sentiment",
+                        sentence, Sentiment.class)
+                .getBody();
+    }
+
+    @GetMapping("/")
+    public String homePage() {
+        return "Spring Boot Sentiment Analysis Web API";
+    }
+
+    @GetMapping("/myname")
+    public String myName() {
+        return "My Name is Priyam";
+    }
+}

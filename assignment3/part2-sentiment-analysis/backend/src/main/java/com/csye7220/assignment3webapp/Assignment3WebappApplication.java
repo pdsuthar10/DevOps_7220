c91545a8f33870b601394be06ba7cc62cff8b84c
Priyam Suthar <suthar.p@northeastern.edu>
2021-10-03 15:09:24 -0400

    Bootstrap Spring Web Application

---
+++ assignment3/part2-sentiment-analysis/backend/src/main/java/com/csye7220/assignment3webapp/Assignment3WebappApplication.java
@@ -0,0 +1,13 @@
+package com.csye7220.assignment3webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment3WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment3WebappApplication.class, args);
+	}
+
+}
---
+++ assignment3/part2-sentiment-analysis/backend/src/test/java/com/csye7220/assignment3webapp/Assignment3WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csye7220.assignment3webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment3WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

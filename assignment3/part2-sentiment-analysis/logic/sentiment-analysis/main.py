ba8a31b8a8bf0c5f1ce1b4047ec367e64c58cad5
Priyam Suthar <suthar.p@northeastern.edu>
2021-10-03 15:42:36 -0400

    Add Flask server to handle the sentiment analysis part

---
+++ assignment3/part2-sentiment-analysis/logic/sentiment-analysis/main.py
@@ -0,0 +1,31 @@
+from textblob import TextBlob
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/testAPI")
+def testAPI():
+    return "Hello from Python Sentiment Analysis Flask App!"
+
+
+@app.route("/analyse/sentiment", methods=['POST'])
+def analyse_sentiment():
+    sentence = request.get_json()['sentence']
+    polarity = TextBlob(sentence).sentences[0].polarity
+    return jsonify(
+        sentence=sentence,
+        polarity=polarity
+    )
+
+
+# use + for spaces, i.e. http://localhost:5000/analyse?sentence=i+am+so+happy!
+@app.route("/analyse", methods=['GET'])
+def analyse_sentiment_get():
+    sentence = request.args.get('sentence')
+    polarity = TextBlob(sentence).sentences[0].polarity
+    return str(polarity)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

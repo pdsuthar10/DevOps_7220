7684791a46ae93d100ffda3277c292a32c116af6
Priyam Suthar <suthar.p@northeastern.edu>
2021-10-04 13:50:31 -0400

    Add Part 1 in assignment 3

---
+++ assignment3/part1-react-ui/rockstar-webapi/Controllers/MusicController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace rockstar_webapi.Controllers
+{
+    [ApiController]
+    public class MusicController : ControllerBase
+    {
+        private readonly Music _myMusic = new Music();
+
+        private readonly ILogger<MusicController> _logger;
+
+        public MusicController(ILogger<MusicController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[controller]")]
+        public String Get()
+        {
+            return "<h1>My Jukebox!</h1>";
+        }
+
+        [HttpGet]
+        [Route("[controller]/o-sanam")]
+        public String GetOSanam()
+        {
+            return _myMusic.OSanam;
+        }
+
+        [HttpGet]
+        [Route("[controller]/laari-chootee")]
+        public String GetLaariChooti()
+        {
+            return _myMusic.LaariChooti;
+        }
+
+        [HttpGet]
+        [Route("[controller]/despacito")]
+        public String GetDespacito()
+        {
+            return _myMusic.Despacito;
+        }
+
+    }
+}
